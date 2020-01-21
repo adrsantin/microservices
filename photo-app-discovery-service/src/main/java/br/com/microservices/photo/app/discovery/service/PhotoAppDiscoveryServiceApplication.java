aa35b959749c1042b937ea66c81463f61af3b56e
Adriano Santin <adr.santin@gmail.com>
2020-01-21 10:26:50 -0300

    Initial commit

---
+++ photo-app-discovery-service/src/main/java/br/com/microservices/photo/app/discovery/service/PhotoAppDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.microservices.photo.app.discovery.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppDiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoAppDiscoveryServiceApplication.class, args);
+    }
+
+}
---
+++ photo-app-discovery-service/src/test/java/br/com/microservices/photo/app/discovery/service/PhotoAppDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.microservices.photo.app.discovery.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
