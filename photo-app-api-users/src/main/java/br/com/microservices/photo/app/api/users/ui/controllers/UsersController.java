5ae7e0013a8d1caf09c07ddaa8ca24117bca81b9
Adriano Santin <adr.santin@gmail.com>
2020-01-27 08:36:48 -0300

    Create create user request model

--- photo-app-api-users/src/main/java/br/com/microservices/photo/app/api/users/ui/controllers/UsersController.java
+++ photo-app-api-users/src/main/java/br/com/microservices/photo/app/api/users/ui/controllers/UsersController.java
@@ -1,8 +1,11 @@
 package br.com.microservices.photo.app.api.users.ui.controllers;
 
+import br.com.microservices.photo.app.api.users.ui.models.CreateUserRequestModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +19,10 @@ public class UsersController {
     @GetMapping("/status/check")
     public String status() {
         return "Working on port " + env.getProperty("local.server.port");
+    }
+
+    @PostMapping
+    public String createUser(@RequestBody CreateUserRequestModel userDetails) {
+        return "Create user method called";
     }
 }
