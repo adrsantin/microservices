0aab1e264cbfa0abd7916f47fa3610b3e5372cf0
Adriano Santin <adr.santin@gmail.com>
2020-02-05 09:15:51 -0300

    Add login authntication

--- photo-app-api-users/src/main/java/br/com/microservices/photo/app/api/users/data/UsersRepository.java
+++ photo-app-api-users/src/main/java/br/com/microservices/photo/app/api/users/data/UsersRepository.java
@@ -3,4 +3,5 @@ package br.com.microservices.photo.app.api.users.data;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UsersRepository extends CrudRepository<UserEntity, Long> {
+    UserEntity findByEmail(String email);
 }
